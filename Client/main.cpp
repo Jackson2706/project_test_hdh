f0e5b551243134b9755e40110146be261bda9a9b
Jackson2706 <dung27062002@gmail.com>
2023-12-12 12:48:18 +0700

    complete for client

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,9 +1,10 @@
 #include <string>
-#include <cpr/cpr.h>
+#include <cpr/cpr.h> // tải
 #include <filesystem>
 #include <iostream>
 #include <string_view>
-#include <nlohmann/json.hpp>
+#include <vector>
+#include <nlohmann/json.hpp> // tải
 using namespace std::filesystem;
 using namespace std;
 using namespace cpr;
@@ -18,7 +19,9 @@ json downloadCRCS(){
     Header headers{{"TokenKey", "aaa"}};
     string url = "http://" + ip + ":" + to_string(port) + "/ListOfAll";
     Response r = Get(Url{url}, headers);
-    return r.text;
+    std::string res = r.text;
+    json res_json = json::parse(res);
+    return res_json;
 }
 
 json scanFolder(){
@@ -33,16 +36,44 @@ json scanFolder(){
 }
 
 json dictReduce( json& masterJson,  json& minorJson) {
-    for(auto& element : minorJson.items()){
-        string key = element.key();
-        if (masterJson.find(key) != masterJson.end()) {
-            std::string minorValueAsString = element.value().dump();
-            std::string masterValueAsString = masterJson[key].dump();
-            std::cout << key << "\t" << masterValueAsString << minorValueAsString <<endl;
-        }
-
+    for (json::iterator it = masterJson.begin(); it != masterJson.end(); ++it) {
+            // Lấy key và value
+            std::string key = it.key();
+            std::string value = it.value();
+            try{
+                if (minorJson.at(key)  == value) {
+                // Erase the element and get the next iterator
+                    it = masterJson.erase(it);
+                }
+            } catch(const std::exception& e){
+                // std::cerr << "Caught exception: " << e.what() << std::endl;
+            }
     }
     return masterJson;
+}
+
+string downloadFile(const string &filepath, const string& ip, int port){
+    Parameters parameters{{"FilePath", filepath}};
+    Header headers{{"TokenKey", "aaa"}};
+
+    auto url = Url{"http://"+ ip+ ":" + to_string(port)+ "/File"};
+    auto response = Get(url, headers, parameters);
+    if (response.error){
+        std::cerr << "Error downloading file: " << response.error.message << std::endl;
+        return "";
+    }
+
+    return response.text;
+}
+
+bool writeDatatoFile(const string& absPath, const string& data){
+    ofstream file(absPath);
+    if(file.is_open()){
+        file << data;
+        file.close();
+        return true;
+    }
+    return false;
 }
 int main(){
     path scriptPath = filesystem::absolute(filesystem::path(__FILE__));
@@ -61,9 +92,39 @@ int main(){
     crcFile = conf.at("crcFile");
     ip = conf.at("ip");
     port = conf.at("port");
-
+    string subToSync = conf.at("subToSync");
     json serverCrcs = downloadCRCS();
     json clientCrcs = scanFolder();
     serverCrcs = dictReduce(serverCrcs, clientCrcs);
+    
+    std::vector<std::string> serverCrcsFilteredKeys;
+
+    for (const auto& entry : serverCrcs.items()) {
+        const std::string& key = entry.key();
+        if(key.find(subToSync) == 0){
+            serverCrcsFilteredKeys.push_back(key);
+        }
+    }
+    
+    int i = 0;
+    string absPath;
+    string data;
+    bool write_status;
+    for (const auto& k: serverCrcsFilteredKeys){
+        i++;
+        absPath = folderPath +"/" + k;
+        cout << "[" << i << "] Start download of: " << k; 
+        data = downloadFile(k, ip,port);
+        write_status = writeDatatoFile(absPath, data);
+
+        if (write_status){
+            cout << " - Status: Success" << endl;
+        } else {
+            cout << " - Status: Fail" << endl;
+        }
+
+        cout << "[" << i << "] End download of: " << k << endl;
+
+    }
     return 0;
 }
