b4125c48c4016e83275089a3340f09e543f2ca0d
Jackson2706 <dung27062002@gmail.com>
2024-01-02 17:56:40 +0700

    clean project

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 g++ -I./Server/include   -o main.o -c main.cpp  -lz -std=c++17
- g++ main.o ./Server/libcrcRoutine.so ./Server/libServer.so ./Client/libcrcRoutine.so ./Client/libClient.so  -o main  -lz -std=c++17
+g++ main.o ./Server/libcrcRoutine.so ./Server/libServer.so ./Client/libcrcRoutine.so ./Client/libClient.so  -o main  -lz -std=c++17
